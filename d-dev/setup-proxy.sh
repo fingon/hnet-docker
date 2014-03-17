bcb90dde651cd5ff398438c17e11f005406f3833
Markus Stenberg <markus.stenberg@iki.fi>
2014-03-17 11:50:47 +0200

    Added dist-upgrade to the Dockerfile.

--- d-dev/setup-proxy.sh
+++ d-dev/setup-proxy.sh
@@ -8,7 +8,7 @@
 # Copyright (c) 2014 cisco Systems, Inc.
 #
 # Created:       Thu Mar 13 11:37:51 2014 mstenber
-# Last modified: Thu Mar 13 13:54:04 2014 mstenber
+# Last modified: Thu Mar 13 15:27:28 2014 mstenber
 # Edit time:     3 min
 #
 
@@ -19,7 +19,6 @@ if [ -n $GW ]
 then
     echo "Acquire::http::Proxy \"http://\"$GW\":8000\";" > /etc/apt/apt.conf.d/30proxy
     apt-get update
-    apt-get -y install git
-    git config --global http.proxy http://$GW:8000
 fi
-
+apt-get -y install git
+[ -n $GW ] && git config --global http.proxy http://$GW:8000
